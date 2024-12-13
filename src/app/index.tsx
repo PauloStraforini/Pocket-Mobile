0855631a6782c336bb345d3084cd8ebef494db8c
PauloStraforini <paulostraforini@gmail.com>
2024-12-13 17:37:38 -0300

    ✨ Update: app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,13 @@
-import { Welcome } from '@/components/welcome'
 import { View, Text } from 'react-native'
+
+import { Welcome } from '@/components/welcome'
+import { Steps } from '@/components/steps'
 
 export default function Index() {
   return (
-    <View 
-    style={{
-    flex: 1, 
-    justifyContent: "center",
-    alignItems: "center", }}
-    >
-     <Text style={{ fontSize: 22 }}>Hello Pauladas !</Text>
+    <View style={{ flex: 1,  padding: 40, gap: 40 }} >
      <Welcome />
+     <Steps />
     </View>
   )
 } 
